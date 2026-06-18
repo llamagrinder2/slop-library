5234f7b98ffe34fd63f1ac38f1c2f840d257702d
llamagrinder2 <rasoati.munkahely@gmail.com>
2026-06-18 18:41:47 +0200

    Add constants for trait values and recommenders

---
+++ src/core/constants.js
@@ -0,0 +1,21 @@
+export const TRAIT_VALUES = ["Volt??", "Poop", "Nem", "Meh", "Igen", "Peak"];
+
+export const TRAIT_ORDER = {
+    Peak: 5,
+    Igen: 4,
+    Meh: 3,
+    Nem: 2,
+    Poop: 1,
+    "Volt??": 0
+};
+
+export const recommenders = {
+    baal: { name1: "The Almighty Baal", name2: "Menace of Bikini Bottom", color: "#FFEB3B" },
+    goatlord: { name1: "Goatlord", name2: "Heir to the Black/Speed Throne", color: "#606060", textColor: "#bbb" },
+    caller: { name1: "Caller of Slam Riffs", name2: "Desecrator of Snare Drums", color: "#1E88E5" },
+    bacchus: { name1: "Bacchus the Second", name2: "the Sodomizer, the Bluntripper", color: "#4CAF50" },
+    mammvth: { name1: "The Mammvth Hvnter", name2: "He Who Preys on Dissodeath", color: "#8D6E63" },
+    gaben: { name1: "X14Gaben88X", name2: "Straightest of all Edges", color: "#AB47BC" },
+    christian: { name1: "Christian Woman", name2: "The wicked wicca", color: "#FF9800" },
+    max: { name1: "Max, the Mad", name2: "Undercover cartel member", color: "#E8E8E8" }
+};
